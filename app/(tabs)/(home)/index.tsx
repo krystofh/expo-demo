39d755ca661b9d173dc58b56046baa924619e276
Krystof Hes <krystof.hes@seznam.cz>
2025-08-28 22:52:52 +0200

    chore: fonts and image

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'expo-router';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
+import Logo from '../../../assets/images/food.png';
 export default function HomeScreen() {
   const [done, setDone] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Link href="/details">View details</Link>
+      <Image source={Logo} style={[styles.img, { width: 100, height: 100 }]} />
+      <Text style={styles.h2} >Home</Text>
+      <Link style={styles.mainText} href="/details">View details</Link>
       <Button title="click me" onPress={() => setDone(true)} />
       {done && <Text>Done</Text>}
     </View>
@@ -17,7 +19,24 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    gap: 5
   },
+
+  img: {
+    marginVertical: 20
+  },
+
+  h1: {
+    fontSize: 30
+  }, 
+
+  h2: {
+    fontSize: 25
+  }, 
+
+  mainText: {
+    fontSize: 20
+  }
 });
